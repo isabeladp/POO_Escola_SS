089363e998b34119f130336ddb13e3668b0c8095
isabeladp <isabeladp98@gmail.com>
2024-09-14 08:52:20 -0300

    Escola Senai

---
+++ demo/src/main/java/projeto_escola_serra/demo/Turma.java
@@ -0,0 +1,17 @@
+package projeto_escola_serra.demo;
+
+public enum Turma {
+    TURMA3("Turma 3001"),
+    TURMA2( "Turma 2001"),
+    TURMA1("Turma 1001");
+
+    private final String turmas;
+
+Turma(String turmas){
+    this.turmas = turmas;
+}
+
+    public String getTurmasEscola(){
+        return turmas;
+    }
+}
